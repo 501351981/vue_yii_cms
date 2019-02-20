2143424daeceb31752ce6caaa6a790c56cafbbd8
liyulin <501351981@qq.com>
2019-02-20 15:25:20 +0800

    add isArray in helper.js

--- src/utils/base/helper.js
+++ src/utils/base/helper.js
@@ -40,5 +40,39 @@ function randomString(len) {
   return pwd;
 }
 
+function getNowFormatDate() {
+  let date = new Date();
+  let seperator1 = "-";
+  let seperator2 = ":";
+  let month = date.getMonth() + 1;
+  let strDate = date.getDate();
+  if (month >= 1 && month <= 9) {
+    month = "0" + month;
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = "0" + strDate;
+  }
+  let currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+    + " " + date.getHours() + seperator2 + date.getMinutes()
+    + seperator2 + date.getSeconds();
+  return currentdate;
+}
 
-export default {phpTimeToJsTime,phpTimeToJsDate,randomString}
+export const isArray=function(obj) {
+  let array=[]
+  return Object.prototype.toString.call(obj)==Object.prototype.toString.call(array)
+}
+
+export const isObject=function(obj) {
+  let object={}
+  return Object.prototype.toString.call(obj)==Object.prototype.toString.call(object)
+}
+
+export const isString=function(str) {
+  let string='a'
+  return Object.prototype.toString.call(str)==Object.prototype.toString.call(string)
+}
+
+
+
+export default {phpTimeToJsTime,phpTimeToJsDate,randomString,getNowFormatDate,isArray,isObject,isString}
